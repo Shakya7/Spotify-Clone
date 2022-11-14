54471804a0c93d9bcb73939354e62f250a89cd36
Shakya7 <shakyasarkar72@gmail.com>
2022-11-14 23:42:27 +0530

    created profile-top-sec v1

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,54 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 
 const Profile = () => {
   return (
     <main className="profile-main">
           <div>
-            Profile
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores ipsam incidunt deserunt architecto debitis magnam repellat sed eveniet atque ea officia illum, aliquam sapiente doloremque, ullam tempore nesciunt rem.</p>
-            <p>Explicabo voluptatem nostrum provident ratione tempore nisi debitis, officia eaque numquam quidem mollitia! Expedita fugit qui consequatur magni hic illum, facilis commodi ea aperiam quibusdam perspiciatis voluptatibus quaerat! Odit, officiis.</p>
-            <p>Error magni dignissimos quaerat in aspernatur voluptatum. Reiciendis inventore ad magnam culpa id, cum illum eum cupiditate ducimus maiores provident eius iure quae consequuntur dolorem repellat aliquam! Tempore, recusandae dolorem.</p>
-          
-            <p>Provident at similique earum, expedita quos, temporibus in dolore architecto molestias consequatur vero quis necessitatibus repudiandae numquam nihil aliquam debitis? Officiis perferendis ullam non dicta laboriosam esse, ad cum consequatur.</p>
-            <p>Laboriosam debitis aliquid officia cupiditate voluptatibus ipsum ipsam quis velit deserunt est expedita tempore ullam quos incidunt natus, eos commodi minus ratione fugiat tempora eius labore? Quibusdam sit vero distinctio?</p>
-            <p>Officia ducimus in necessitatibus? Ipsa quae, veniam ut optio perferendis eligendi id ab quos excepturi vero possimus ex fuga eaque quas expedita soluta harum dolor sunt repellat laboriosam obcaecati? Nobis.</p>
+            <div>
+              <div className="profile-big-icon">
+                <FontAwesomeIcon icon={faUser}/>
+              </div>
+              <div className="profile-top-info">
+                <p>PROFILE</p>
+                <p>Shakya</p>
+              </div>
+            </div>
           </div>
-          <p>Perferendis culpa quas ipsum voluptate unde, ducimus adipisci maiores, excepturi optio, saepe voluptatum blanditiis. Sequi eius corrupti laboriosam, assumenda voluptatibus, id aliquid qui, a totam dolores atque facere ducimus odit?</p>
-          <p>Incidunt vero, at nobis odit in debitis sit aliquam tempore cumque culpa iure repellat quas ipsum pariatur suscipit reiciendis beatae harum nostrum quod magni facere? Eum quas adipisci nostrum nemo!</p>
-          <p>Non ipsum hic nisi magnam, rerum facere soluta alias qui accusamus earum veritatis praesentium quas dolorum aspernatur inventore excepturi ipsam nulla totam odit nam cupiditate exercitationem reprehenderit consequuntur optio. Sunt!</p>
-          <p>Optio eos officia nulla. Eveniet, praesentium id illum quibusdam maiores soluta amet fugit voluptatem in illo explicabo omnis fugiat exercitationem dignissimos, dolorem aliquam? Quo odio placeat impedit perspiciatis nihil rerum.</p>
-          <p>Reiciendis eaque similique odit nemo ullam recusandae cupiditate nobis corporis sed. Quod, delectus voluptatum. Excepturi alias explicabo, ullam voluptatum deleniti omnis voluptatem perspiciatis, aperiam error minima officiis amet? Accusantium, a!</p>
-          <p>Ipsum, iusto architecto non magni officia adipisci at accusantium, sequi alias deserunt error voluptas minima facere deleniti unde laboriosam. Nostrum similique debitis culpa pariatur nihil tenetur distinctio placeat blanditiis optio.</p>
-          <p>Dolore, voluptatum nobis. Quia repellat quidem magnam dignissimos voluptatem quibusdam, odio voluptatum soluta temporibus nam. Tempore eaque ipsum aliquid id, quia doloribus amet facilis voluptate, fugit obcaecati eos, possimus rem.</p>
-          <p>Consequuntur reprehenderit dicta veritatis saepe, eius placeat ullam aspernatur magnam illo voluptates quas numquam non repellat repudiandae, rem nulla cumque nam veniam ea eligendi quos delectus. Odio vel fugit error.</p>
-          <p>Blanditiis eligendi illo excepturi quis illum deleniti iure, ipsa provident tenetur modi quasi! Assumenda doloremque cum maiores iure illum in reiciendis eos enim est sint repellat excepturi, vitae ut maxime!</p>
-          <p>Eligendi, ut ex adipisci veniam, cupiditate cum quisquam tempora illo debitis esse dolorem velit ipsam hic. Eveniet, neque dicta! Aut, vero voluptatibus sit dolore quos quisquam reprehenderit ipsam repellat atque.</p>
-          <p>Nisi ducimus eos temporibus debitis repellendus asperiores amet vero repellat nam saepe optio sequi, reiciendis aut iusto? Tenetur quia placeat magnam. Deserunt quisquam eveniet suscipit in debitis quam quasi temporibus!</p>
-          <p>Distinctio mollitia sit aspernatur numquam nam repellat, et neque odit modi dolor voluptates error. Dolore ducimus sequi ipsa, reiciendis molestias delectus quaerat laudantium reprehenderit sunt magni, deserunt nesciunt repellat minima!</p>
-          <p>Explicabo, minus in labore numquam officiis voluptatibus dolorum? Corporis, in aliquam! Voluptatum alias minima amet quo natus accusantium beatae sint eligendi aut, saepe vel rerum, enim nostrum reprehenderit aspernatur molestias.</p>
-          <p>Ea optio similique impedit enim necessitatibus quasi numquam reprehenderit! Pariatur dicta facere optio sit minus quisquam? Cum reiciendis architecto nisi! Doloremque nesciunt maiores rerum officiis voluptate, fugit hic. Voluptas, vel.</p>
-          <p>Temporibus ducimus alias amet accusantium. Delectus, deleniti. Provident doloremque nam, velit perferendis vel dolores, reprehenderit ducimus molestias odio assumenda fuga, doloribus molestiae. Harum eius quam cum adipisci dignissimos iure eaque.</p>
-          <p>Assumenda amet ullam laborum praesentium neque nam debitis, vero itaque eius facilis officia minima maxime pariatur cumque aliquid commodi atque qui esse, voluptates mollitia quod reprehenderit quaerat laboriosam quas. Sunt.</p>
-          <p>Earum, dolor nesciunt! Eum quisquam reprehenderit ducimus animi illum inventore natus fuga vero harum unde, blanditiis voluptatibus obcaecati tempore soluta corrupti laboriosam quo rem optio reiciendis delectus modi possimus et?</p>
-          <p>Laudantium iusto neque sunt nostrum, ipsum cumque nulla, officiis, deleniti ipsa natus eligendi. Corporis, pariatur ut voluptas vitae laudantium dolorem animi nostrum obcaecati doloribus quis ullam sunt aspernatur tenetur esse.</p>
-          <p>Dolorem quaerat obcaecati animi enim quis natus, eaque quibusdam nisi, velit ea deserunt harum numquam minima reprehenderit perspiciatis quam blanditiis, rerum impedit! Aspernatur quidem architecto esse soluta, earum molestias ad?</p>
-          <p>Nam commodi nostrum, assumenda debitis perferendis consequatur ab doloremque tempore temporibus dicta soluta cumque cupiditate esse, nobis praesentium accusamus voluptate porro labore culpa enim voluptas! Veritatis recusandae cupiditate nulla pariatur.</p>
-          <p>Ex praesentium incidunt beatae nisi inventore quae culpa neque quisquam temporibus. Soluta accusamus, eum magni nisi, totam tempore perspiciatis praesentium explicabo ab quisquam deserunt aut eaque vel ullam similique deleniti!</p>
-          <p>Recusandae dignissimos incidunt maiores mollitia reiciendis exercitationem, eligendi quaerat ipsa beatae, odit amet facere rem natus nesciunt optio, inventore provident adipisci quam dolorem placeat. Sunt vitae deserunt quidem debitis minima?</p>
-          <p>Delectus sint illo tempora pariatur dolores voluptatibus possimus vel dolore odit, reprehenderit veritatis voluptas, fugit eaque? Dolor ipsam voluptate nobis minus enim! Consequuntur rem, explicabo velit excepturi vero placeat aliquam?</p>
-          <p>Cupiditate ex quae quasi aut architecto optio illum at doloremque dolore tempora voluptatum est mollitia voluptas error incidunt facilis, nam assumenda, explicabo in accusamus expedita odio totam blanditiis quo. Omnis.</p>
-          <p>Velit ex error, voluptatum, sit blanditiis sequi, ipsum consequatur possimus repellendus quas asperiores. Ipsam corrupti quae quis culpa! Ipsam iste sapiente voluptatibus cumque eveniet nostrum beatae minus soluta tenetur praesentium!</p>
-          <p>Reiciendis unde id obcaecati rem est sapiente quasi tenetur deleniti! Tenetur iusto quia distinctio, dolorum aut earum pariatur unde possimus repellendus quidem ducimus doloribus cum in assumenda voluptates quos expedita.</p>
-          <p>Suscipit rerum consequatur quidem quasi aperiam amet modi in explicabo nemo, iusto doloremque praesentium dicta repellat aliquam quibusdam beatae. Exercitationem dignissimos doloribus dicta nesciunt repudiandae iste beatae harum. Impedit, deleniti.</p>
-          <p>Exercitationem consectetur nesciunt sed amet qui totam excepturi dolore ratione iure! Perspiciatis dolorum, explicabo pariatur minus quaerat aut cum asperiores id reprehenderit tempore, hic atque quas vitae fugiat illo autem?</p>
-          <p>Praesentium ullam voluptates eaque consequuntur commodi eos qui. Vero dicta qui, at minima consequuntur harum aperiam. Rerum a blanditiis sit. Distinctio excepturi eum dicta veniam exercitationem adipisci, provident atque rem!</p>
-          <p>Assumenda alias saepe quaerat distinctio vel dolor delectus sed eligendi est dicta obcaecati numquam, adipisci, praesentium voluptatem! Ipsa voluptatum assumenda quasi, earum dicta distinctio, delectus illum porro corrupti aspernatur totam.</p>
-          <p>Omnis pariatur voluptate delectus, nemo ab molestiae, beatae explicabo itaque nihil tenetur iusto eaque distinctio voluptatem error, minus qui aliquid quisquam aliquam hic voluptatum placeat laboriosam architecto! Similique, numquam qui.</p>
-          <p>Dicta asperiores vitae labore beatae in! Nisi debitis deserunt, consectetur nam quo officiis adipisci. Numquam, velit voluptatum soluta tempora culpa voluptate facere animi repudiandae enim error debitis. Omnis, distinctio quod!</p>
-          <p>Obcaecati impedit illo aliquid eaque ducimus dicta vero enim quibusdam dolorem id laborum, laudantium molestiae consequatur expedita corrupti vel maxime cupiditate. Dolore a iusto tempore ipsam tempora aspernatur nihil cum?</p>
-          <p>Pariatur commodi possimus fugit eligendi placeat, deserunt natus earum ex labore necessitatibus quidem harum saepe iure quam asperiores quasi aut explicabo nobis dignissimos provident debitis sint. Doloremque, distinctio optio! Iusto.</p>
-          <p>Facere optio reiciendis asperiores magni, distinctio atque. Impedit accusamus exercitationem veniam officiis aperiam delectus nihil ea maxime nemo illum, quae cupiditate provident? Pariatur repudiandae voluptatibus, quisquam cupiditate molestiae nesciunt veniam.</p>
-          <p>Numquam molestias similique deserunt nobis repellat omnis fuga aspernatur quod impedit repellendus illum facilis iste delectus assumenda eaque eum facere voluptatibus eligendi illo sapiente, recusandae labore. Aspernatur natus assumenda ipsum!</p>
+          <p>Dfdfd</p>
+          
         
     </main>
   )
